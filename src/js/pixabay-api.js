4c502bd08b6a01ce0baee440b8c4e8ffe58198b0
v.karakai <v.karakai@pin-up.tech>
2025-03-23 12:19:52 +0200

    try to use github env

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const API_KEY = process.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = 'https://pixabay.com/api/';
 const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
